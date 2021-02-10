4c959428f10bad80b32f1a4d8d9fa8e2eac4165c
rubenperezf <rubenperezfeijoo@gmail.com>
2021-02-10 16:03:00 -0500

    Updates

---
+++ precache-manifest.d3b11133ef42c7eacb8257e32ce73d05.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0e70e5a91c7d6a1cd49bc222c312422",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a8e63dffbdb695a9c3d7",
+    "url": "./static/css/2.c9a2d852.chunk.css"
+  },
+  {
+    "revision": "b617c4a60c85c02c6250",
+    "url": "./static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "a8e63dffbdb695a9c3d7",
+    "url": "./static/js/2.76be11d5.chunk.js"
+  },
+  {
+    "revision": "6472a7d1d95598048b2f7b02095da167",
+    "url": "./static/js/2.76be11d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b617c4a60c85c02c6250",
+    "url": "./static/js/main.43a2c0f1.chunk.js"
+  },
+  {
+    "revision": "dccd92b092cb67ce2ec0",
+    "url": "./static/js/runtime-main.f334f5e0.js"
+  }
+]);
